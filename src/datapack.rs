a9a6e4dcb452bc137d3f24bd747a09556165665f
rudyon <keremberket@gmail.com>
2026-09-23 13:31:44 +0300

    exporting

---
+++ src/datapack.rs
@@ -0,0 +1,91 @@
+use crate::fields::Sample;
+use image::{GrayImage, ImageBuffer, RgbaImage};
+use serde_json::{json, to_string_pretty};
+use std::{fs, path::Path};
+
+pub fn export_datapack(path: &str, world_size: usize, elevation: &Sample, biomes: Vec<[u8; 4]>) {
+    let pack_mcmeta = json!({
+        "pack": {
+            "description": "Fictrix",
+            "min_format": 121,
+            "max_format": 121
+        }
+    });
+
+    let dimension = json!({
+        "type": "minecraft:overworld",
+        "generator": {
+            "type": "novoatlas:image_map",
+            "map_info": "fictrix:overworld",
+            "settings": "minecraft:overworld",
+            "underground_density_function": "novoatlas:caves",
+            "biome_source": {
+                "type": "novoatlas:biome_cell_color_map",
+                "map_info": "fictrix:overworld",
+                "default_biome": "minecraft:the_void"
+            }
+        }
+    });
+
+    let map_info = json!({
+        "height_map": "fictrix:overworld",
+        "starting_y": 0,
+        "surface_biomes": {
+            "map": "fictrix:overworld",
+            "biomes": [
+                {
+                    "biome": "minecraft:plains",
+                    "color": "#00ff00"
+                },
+                {
+                    "biome": "minecraft:ocean",
+                    "color": "#0000ff"
+                }
+            ]
+        }
+    });
+
+    let heightmap: GrayImage =
+        ImageBuffer::from_fn(world_size as u32, world_size as u32, |x, y| {
+            let index = (y as usize) * world_size + (x as usize);
+            let value = elevation.array[index];
+            let pixel_value = (value * 255.0) as u8;
+            image::Luma([pixel_value])
+        });
+
+    let biomemap: RgbaImage = ImageBuffer::from_fn(world_size as u32, world_size as u32, |x, y| {
+        let index = (y as usize) * world_size + (x as usize);
+        let pixel_value = biomes[index];
+        image::Rgba(pixel_value)
+    });
+
+    let export_path = Path::new(path);
+
+    fs::create_dir_all(export_path.join("data/minecraft/dimension")).unwrap();
+    fs::create_dir_all(export_path.join("data/fictrix/novoatlas/map_info")).unwrap();
+    fs::create_dir_all(export_path.join("data/fictrix/novoatlas/heightmap")).unwrap();
+    fs::create_dir_all(export_path.join("data/fictrix/novoatlas/biome_map")).unwrap();
+
+    fs::write(
+        export_path.join("pack.mcmeta"),
+        to_string_pretty(&pack_mcmeta).unwrap(),
+    )
+    .unwrap();
+    fs::write(
+        export_path.join("data/minecraft/dimension/overworld.json"),
+        to_string_pretty(&dimension).unwrap(),
+    )
+    .unwrap();
+    fs::write(
+        export_path.join("data/fictrix/novoatlas/map_info/overworld.json"),
+        to_string_pretty(&map_info).unwrap(),
+    )
+    .unwrap();
+
+    heightmap
+        .save(export_path.join("data/fictrix/novoatlas/heightmap/overworld.png"))
+        .unwrap();
+    biomemap
+        .save(export_path.join("data/fictrix/novoatlas/biome_map/overworld.png"))
+        .unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,37 @@
+mod datapack;
 mod fields;
-use fields::{OpenSimplex, normalize, sample};
-use image::{GrayImage, ImageBuffer};
+use datapack::export_datapack;
+use fields::{OpenSimplex, Sample, normalize, sample};
 
-fn generate_world(seed: u32, world_size: usize) -> Vec<f32> {
-    let noise = OpenSimplex::new(seed, 16.0);
+fn generate_elevation(seed: u32, world_size: usize) -> Sample {
+    let noise = OpenSimplex::new(seed, 32.0);
 
     let elevation = sample(&noise, world_size, world_size);
     let elevation_normalized = normalize(&elevation);
 
-    elevation_normalized.array
+    elevation_normalized
+}
+
+fn generate_biomes(elevation: &Sample) -> Vec<[u8; 4]> {
+    elevation
+        .array
+        .iter()
+        .map(|&value| {
+            if value < 0.3 {
+                [0, 0, 255, 255] // minecraft:ocean
+            } else {
+                [0, 255, 0, 255] // minecraft:plains
+            }
+        })
+        .collect::<Vec<[u8; 4]>>()
 }
 
 fn main() {
     let seed = 0;
-    let world_size = 1600;
+    let world_size = 256;
 
-    let elevation = generate_world(seed, world_size);
+    let elevation = generate_elevation(seed, world_size);
+    let biomes = generate_biomes(&elevation);
 
-    let heightmap: GrayImage =
-        ImageBuffer::from_fn(world_size as u32, world_size as u32, |x, y| {
-            let index = (y as usize) * world_size + (x as usize);
-            let value = elevation[index];
-            let pixel_value = (value * 255.0) as u8;
-            image::Luma([pixel_value])
-        });
-
-    heightmap
-        .save("heightmap.png")
-        .expect("Failed to save heightmap");
+    export_datapack("output/Fictrix", world_size, &elevation, biomes);
 }
